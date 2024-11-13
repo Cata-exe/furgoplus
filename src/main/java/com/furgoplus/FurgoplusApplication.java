1b76149bd452ed4519e730e8b03586032821206d
Cata-exe <catalina.hernandezf5@gmail.com>
2024-11-13 17:37:26 -0300

    primer commit

---
+++ src/main/java/com/furgoplus/FurgoplusApplication.java
@@ -0,0 +1,13 @@
+package com.furgoplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurgoplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurgoplusApplication.class, args);
+	}
+
+}
